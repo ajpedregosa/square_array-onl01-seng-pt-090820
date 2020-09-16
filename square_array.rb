3550a09ec3092aa964a0fef344fba9c93c0e426b
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-16 14:17:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   return squares = []
-  number = [1, 2, 3]
-  number.each do |num|
+  array = [1, 2, 3]
+  array.each do |num|
     squares << (num * num)
   end
   puts squares
